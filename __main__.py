ac5cc4baa6f05e0be32a62b37738ba3abf9a570e
Neo <160586206+NeoZett@users.noreply.github.com>
2025-02-08 21:26:32 +0100

    Added files

---
+++ __main__.py
@@ -0,0 +1,13 @@
+"""
+NET.PY - A simple way to authorize and share internet connections.
+
+Ever wanted a easy, secure, and efficient way to share data between 
+one device and the other? Well, now you can! In fact, it's super 
+easy with our server and client architectures. Give it a try!
+
+VERSION 1.0.0
+BY Neo Zetterberg (From sweden)
+"""
+
+from .authenticator import SERVER, CLIENT
+from .network import SecureMessage, simple_socket, Server, Client, generate_key, generate_hmac_key
